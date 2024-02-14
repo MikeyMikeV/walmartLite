2b6585bd677b83e5f14ea63c9b60a8c404657145
Mikey Mike <33062798+MikeyMikeV@users.noreply.github.com>
2024-02-14 22:08:27 +0600

    booger

---
+++ shopApp/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import *
+from mptt.admin import DraggableMPTTAdmin
+
+class CategoryAdmin(DraggableMPTTAdmin):
+    list_display = ('tree_actions','indented_title')
+    list_display_links = ('indented_title',)
+    # ordering = ('pk',)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product)
+admin.site.register(Manufacturer)
+admin.site.register(ProductImage)
+admin.site.register(Brand)
+
+# Продукты
+#     Конфеты
+#         Шоколат
+#             Бренды: 3 штуки
+#             Товары: 5-9 штук
